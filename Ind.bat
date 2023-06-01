fe200be137f4844aaa7e815aa98687aba6f81a07
KondratevaDarina <132542993+KondratevaDarina@users.noreply.github.com>
2023-06-01 07:07:12 +0300

    Add files via upload

---
+++ Ind.bat
@@ -0,0 +1,19 @@
+echo off
+md Kondrateva
+cd Kondrateva
+md Darina
+md Dmitrievna
+echo > 12072002.txt
+cd Dmitrievna
+echo > SvoyNout.txt
+pause
+cd ..
+cd ..
+del Kondrateva /S/Q/F
+pause
+cd Kondrateva
+rd Darina
+rd Dmitrievna
+cd..
+rd Kondrateva
+pause
